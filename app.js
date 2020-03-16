21df8a57bdd0ba9422ba97ea699435eff7518c13
unknown <infinity_michael@yahoo.com>
2020-03-15 17:59:10 -0700

    connected to db

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const moongoose = require("moongoose");
 
 const app = express();
 
@@ -9,12 +10,19 @@ const app = express();
 
 // Routes
 app.get("/", (req, res) => {
-  res.send("welcome");
+    res.send("welcome");
 });
 
 app.get("/posts", (req, res) => {
-  res.send("welcome to post page");
+    res.send("welcome to post page");
 });
+
+
+// connect to DB
+moongoose.connect('mongodb+srv://cmcwebcode:cmcWebCode7@cluster0-kactq.mongodb.net/test', () => {
+        console.log("connected to db")
+    })
+    // console.log("hello");
 
 // How to we start listening to the sevrer
 app.listen(3000);
