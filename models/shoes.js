51095325efb22bad2cfd7b6e616ce3503f2e67c1
unknown <infinity_michael@yahoo.com>
2020-04-19 12:29:34 -0700

    added the bags and shoes models

---
+++ models/shoes.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const ShoesSchema = mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('shoeshowcases', ShoesSchema);
