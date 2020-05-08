95bcf5746d0fb2339e534415042de9cf89a9c5d0
unknown <infinity_michael@yahoo.com>
2020-05-08 11:30:35 -0700

    fix: added config setting to heroku

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
-// const cors = require('cors');
+const cors = require('cors');
 
-// app.use(cors());
+module.exports = app;
+app.use(cors());
 dotenv.config();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -22,9 +23,7 @@ app.get('/', (req, res) => {
   res.send('welcome');
 });
 
-// const uri = 'mongodb+srv://mike_123:<chi2000>@cluster0-ksjh1.mongodb.net/test?retryWrites=true&w=majority';
 
-// mongoose.connect('mongodb://localhost:27017/onlineshop', { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.connect('mongodb+srv://mike_123:chi2000@cluster0-ksjh1.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.listen(4001);
